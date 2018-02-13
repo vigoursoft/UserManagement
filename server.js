c79a6f9ee332ae251a3c4cd8b3cccc79fa22f465
vigoursoft <hans@vigoursoft.com>
2018-02-13 11:24:09 +0100

    Add files via upload
    added config.txt for email verification

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use(express.static('public'));
 app.use(express.static('scripts'));
 app.use(express.static('styles'));
 
+var dict=processFile('config.txt');
+
+
 //incoming link click from the user's email address
 app.get('/register', function(request, response){
  
@@ -38,13 +41,14 @@ app.get('/register', function(request, response){
   /* retrieve the email addy from index html form, create  
   and store a temp id/PIN; send email invitation link */
   app.post('/send-mail', function (request, response) {
+    console.log('done reading file.'+dict.port);
     let transporter = nodeMailer.createTransport({
-        host: 'your smpt host',
-        port: 587,
+        host: ''+dict.host+'',
+        port: parseInt(dict.port),
         secure: false,
         auth: {
-            user: 'your email addy',
-            pass: 'your password'
+            user: ''+dict.user+'',
+            pass: ''+dict.pass+''
         },
         tls:{
             ciphers:'SSLv3'
@@ -53,10 +57,10 @@ app.get('/register', function(request, response){
 
     randNumber=Math.floor((Math.random()*100000) + 10000); console.log(randNumber);
    let mailOptions = {
-        from: '"You" <your email>', // sender address
+        from: dict.from, // sender address
         to: request.body.recipient, // list of receivers
-        subject: 'Register', // Subject line
-       // change server
+        subject: dict.register, // Subject line
+       // text: request.body.body, // plain text body
         html: '<a href="http://localhost:3000/register?recipient=' +request.body.recipient
             +'&id='+randNumber+'"<b>Please click this link to continue registration</b></a><p>'+
             "Your PIN number is "+ randNumber // html body
@@ -120,6 +124,36 @@ function dbops(sql, msg){
       });
 
   return "okay";
+}
+
+
+function processFile(inputFile) {
+    var dict={};
+    var first, second, idx;
+
+    var fs = require('fs'),
+        readline = require('readline'),
+        instream = fs.createReadStream(inputFile),
+        outstream = new (require('stream'))(),
+        rl = readline.createInterface(instream, outstream);
+     
+    rl.on('line', function (line) {
+        idx=line.indexOf(":");
+        first=line.substring(0, idx);
+        idx=line.indexOf(":")+1;
+        second=line.substring(idx);
+        dict[first]=second;
+        console.log("first is "+first +" second is "+second);
+        
+    });
+    
+    rl.on('close', function (line) {      
+        console.log('done reading file.'+dict.host);
+       
+    });
+
+    return dict;
+   
 }
 
 // do it
